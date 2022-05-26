19e36504d80dacb472a6e3116ff7f28909a9119f
Linas Kucinskas <linas.kucinskas@gmil.com>
2022-05-26 19:04:04 +0300

    05-26

---
+++ 2022-05-26/Strings/script.js
@@ -0,0 +1,10 @@
+let userInfoOutput = document.getElementById('main');
+
+let user = {
+    firstName: "Vardas",
+    lastName: "Pavarde"
+  };
+
+  userInfoOutput.innerHTML =
+	`<h2>User Information:</h2>` +
+	`<p>User name: ${user.firstName}, Last name: ${user.lastName}.</p>`;
